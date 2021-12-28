5b26e799ee9a1ee99750b2a1b761474fcf479143
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-12-28 13:10:13 +0900

    코드 재구성

--- 01.Swing기본/src/hw/PhoneBookFrame.java
+++ 01.Swing기본/src/hw/PhoneBookFrame.java
@@ -15,30 +15,18 @@ import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 
 public class PhoneBookFrame extends JFrame {
-	//JToggleButton
-	private JToggleButton generalButton;
-	private JToggleButton universityButton;
-	private JToggleButton companyButton;
+	private JToggleButton generalButton, universityButton, companyButton;
 	
-	//JButton
-	private JButton inputButton;
-	private JButton resetButton;
+	private JButton inputButton, resetButton;
 	
-	//JTextField
-	private JTextField nameField;
-	private JTextField phoneNumField;
-	private JTextField birthDayField;
+	private JTextField nameField, phoneNumField, birthDayField;
 	
-	//Additional JTextField
-	private JTextField majorField;
-	private JTextField gradeField;
-	
+	private JTextField majorField, gradeField;
+
 	private JTextField companyField;
 	
-	//CardLayout
 	private JPanel additionalFormPanel;
 	private CardLayout cardLayout;
-	
 	
 
 	public PhoneBookFrame() 
@@ -54,16 +42,22 @@ public class PhoneBookFrame extends JFrame {
 	}
 	
 	
-	
 	private void buildGUI() 
 	{
-		this.setLayout(new BorderLayout());
-		
-		this.add(createTopButtonPanel(), BorderLayout.NORTH);
-		this.add(createCenterPanel(), BorderLayout.CENTER);
-		this.add(createBottomButtonPanel(), BorderLayout.SOUTH);
+		this.add(getInfoPanel());
 	}
-	
+
+
+	private JPanel getInfoPanel()
+	{
+		JPanel panel = new JPanel(new BorderLayout());
+		
+		panel.add(createTopButtonPanel(), BorderLayout.NORTH);
+		panel.add(createCenterPanel(), BorderLayout.CENTER);
+		panel.add(createBottomButtonPanel(), BorderLayout.SOUTH);
+		
+		return panel;
+	}
 	
 	
 	private JPanel createTopButtonPanel()
@@ -89,7 +83,7 @@ public class PhoneBookFrame extends JFrame {
 	}
 	private JPanel createCenterPanel()
 	{
-		JPanel panel = new JPanel(new GridLayout(2, 1));
+		JPanel panel = new JPanel();
 		
 		panel.add(createForm());
 		
@@ -109,7 +103,6 @@ public class PhoneBookFrame extends JFrame {
 	}
 	
 	
-	
 	private JPanel createForm()
 	{
 		JPanel panel = new JPanel(new BorderLayout());
@@ -122,11 +115,28 @@ public class PhoneBookFrame extends JFrame {
 	}
 	private JPanel createDefaultForm()
 	{
-		JPanel panel = new JPanel(new GridLayout(3, 1));
+		JPanel panel = new JPanel(new GridLayout(0, 1));
 		
-		panel.add(createNameForm());
-		panel.add(createPhoneNumForm());
-		panel.add(createBirthDayForm());
+		JPanel p_name = new JPanel();
+		JPanel p_phoneNum = new JPanel();
+		JPanel p_birthDay = new JPanel();
+		
+		nameField = new JTextField(10);
+		phoneNumField = new JTextField(10);
+		birthDayField = new JTextField(10);
+		
+		p_name.add(new JLabel("이        름"));
+		p_name.add(nameField);
+		
+		p_phoneNum.add(new JLabel("전화번호"));
+		p_phoneNum.add(phoneNumField);
+		
+		p_birthDay.add(new JLabel("생년월일"));
+		p_birthDay.add(birthDayField);
+		
+		panel.add(p_name);
+		panel.add(p_phoneNum);
+		panel.add(p_birthDay);
 		
 		return panel;
 	}
@@ -137,145 +147,58 @@ public class PhoneBookFrame extends JFrame {
 		additionalFormPanel = new JPanel();
 		additionalFormPanel.setLayout(cardLayout);
 		
-		additionalFormPanel.add("general", generalPanel());
-		additionalFormPanel.add("university", universityPanel());
-		additionalFormPanel.add("company", companyPanel());
+		JPanel p_general = new JPanel(new GridLayout(0, 1));
+		JPanel p_university = new JPanel(new GridLayout(0, 1));
+		JPanel p_company = new JPanel(new GridLayout(0, 1));
 		
-		cardLayout.show(additionalFormPanel, "general");
+		JPanel p_p_university_1 = new JPanel();
+		JPanel p_p_university_2 = new JPanel();
+		JPanel p_p_company = new JPanel();
+		
+		majorField = new JTextField(10);
+		gradeField = new JTextField(10);
+		
+		companyField = new JTextField(10);
+		
+		
+		p_p_university_1.add(new JLabel("전        공"));
+		p_p_university_1.add(majorField);
+		p_p_university_2.add(new JLabel("학        년"));
+		p_p_university_2.add(gradeField);
+		
+		p_p_company.add(new JLabel("회        사"));
+		p_p_company.add(companyField);
+
+		p_university.add(p_p_university_1);
+		p_university.add(p_p_university_2);
+		p_company.add(p_p_company);
+		
+		additionalFormPanel.add("일반", p_general);
+		additionalFormPanel.add("대학", p_university);
+		additionalFormPanel.add("회사", p_company);
+		
+		//Default로 제일 먼저 추가한 "general"이 처음에 보임.
+		//cardLayout.show(additionalFormPanel, "general");
 		
 		return additionalFormPanel;
 	}
 	
 	
-	
-	private JPanel createNameForm()
-	{
-		JPanel panel = new JPanel();
-		
-		JLabel label = new JLabel("이        름");
-		nameField = new JTextField(10);
-		
-		panel.add(label);
-		panel.add(nameField);
-		
-		return panel;
-	}
-	private JPanel createPhoneNumForm()
-	{
-		JPanel panel = new JPanel();
-		
-		JLabel label = new JLabel("전화번호");
-		phoneNumField = new JTextField(10);
-		
-		panel.add(label);
-		panel.add(phoneNumField);
-		
-		return panel;
-	}
-	private JPanel createBirthDayForm()
-	{
-		JPanel panel = new JPanel();
-		
-		JLabel label = new JLabel("생년월일");
-		birthDayField = new JTextField(10);
-		
-		panel.add(label);
-		panel.add(birthDayField);
-		
-		return panel;
-	}
-	
-	
-	
-	private JPanel generalPanel()
-	{
-		JPanel panel = new JPanel(new GridLayout(2, 1));
-		
-		return panel;
-	}
-	private JPanel universityPanel()
-	{
-		JPanel panel = new JPanel(new GridLayout(2, 1));
-		
-		panel.add(createMajorForm());
-		panel.add(createGradeForm());
-		
-		return panel;
-	}
-	private JPanel companyPanel()
-	{
-		JPanel panel = new JPanel(new GridLayout(2, 1));
-		
-		panel.add(createCompanyForm());
-		
-		return panel;
-	}
-	
-	
-	
-	private JPanel createMajorForm()
-	{
-		JPanel panel = new JPanel();
-		
-		JLabel label = new JLabel("전        공");
-		majorField = new JTextField(10);
-		
-		panel.add(label);
-		panel.add(majorField);
-		
-		return panel;
-	}
-	private JPanel createGradeForm()
-	{
-		JPanel panel = new JPanel();
-		
-		JLabel label = new JLabel("학        년");
-		gradeField = new JTextField(10);
-		
-		panel.add(label);
-		panel.add(gradeField);
-		
-		return panel;
-	}
-	private JPanel createCompanyForm()
-	{
-		JPanel panel = new JPanel();
-		
-		JLabel label = new JLabel("회        사");
-		companyField = new JTextField(10);
-		
-		panel.add(label);
-		panel.add(companyField);
-		
-		return panel;
-	}
-	
 	private void registerEvent()
 	{
-		generalButton.addActionListener(new ActionListener() {
+		ActionListener handler = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardLayout.show(additionalFormPanel, e.getActionCommand());
+				
+			}
 			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(additionalFormPanel, "general");
-				
-			}
-		});
-		universityButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(additionalFormPanel, "university");
-				
-			}
-		});
-		companyButton.addActionListener(new ActionListener() {
-	
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(additionalFormPanel, "company");
-				
-			}
-		});
+		};
+		
+		generalButton.addActionListener(handler);
+		universityButton.addActionListener(handler);
+		companyButton.addActionListener(handler);
 		
 		inputButton.addActionListener(new ActionListener() {
 
@@ -289,7 +212,7 @@ public class PhoneBookFrame extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(additionalFormPanel, "general");
+				cardLayout.show(additionalFormPanel, "일반");
 				generalButton.setSelected(true);
 				
 				nameField.setText("");
